a8e1172fbd1af84324af4929ca8accdab49ce543
Bùi Tiến Đình <miktendinh@gmail.com>
2022-03-12 18:25:31 +0700

    Buoi 3

---
+++ src/components/ShowInfo.tsx
@@ -0,0 +1,13 @@
+type ShowInfoProps = {
+    name: string,
+    age: number
+}
+
+const ShowInfo = (props: ShowInfoProps) => {
+    return <div>
+        {props.name}
+        {props.age}
+    </div>
+    
+}
+export default ShowInfo;
